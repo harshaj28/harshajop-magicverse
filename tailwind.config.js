0d34f856078f53a1857065e81095c33a0bb43da9
Harshaj Singh Parihar <62892079+harshaj28@users.noreply.github.com>
2022-01-15 14:26:04 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
+  purge: ["./pages/**/*.js", "./components/**/*.js"],
   content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
